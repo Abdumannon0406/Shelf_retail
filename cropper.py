b53c35435d8ac9a450b07484a1ba277bdf75cedc
Abdumannon0406 <halimovabdumannon0406@gmail.com>
2025-12-11 17:49:08 +0500

    Cropping of shelf

--- cropper.py
+++ cropper.py
@@ -3,24 +3,22 @@ from pathlib import Path
 import os,cv2
 model=YOLO('runs/detect/train7/weights/best.pt')
 
-image_path=list(Path('/mnt/data11/projects/Shelf_retail/DATASET/Shelf_detection_dataset/images/train').glob('*.jpg'))
-# print(len(image_path))
-output_folder="/mnt/data11/projects/Shelf_retail/DATASET/Cropped_shelf"
+image_path=('Pasted image.png')
+# print(len(image_path))images/train
+output_folder="/mnt/data11/projects/Shelf_retail/DATASET/Cropped_shelf_test"
 os.makedirs(output_folder,exist_ok=True)
 
-for i in range(len(image_path)):
+resutls=model.predict(image_path)
 
-    resutls=model.predict(image_path[i])
+keypoints=resutls[0].boxes.xyxy.cpu().numpy().astype(int)
+# points=bbox['xyxy']
+# print(len(keypoints))
+image_stem=Path(image_path).stem
 
-    keypoints=resutls[0].boxes.xyxy.cpu().numpy().astype(int)
-    # points=bbox['xyxy']
-    # print(len(keypoints))
-    image_stem=image_path[i].stem
+image=cv2.imread(image_path,cv2.COLOR_BGR2RGB)
 
-    image=cv2.imread(image_path[i],cv2.COLOR_BGR2RGB)
-
-    for j in range(len(keypoints)):
-        cropped_image=image[keypoints[j][1]:keypoints[j][3],keypoints[j][0]:keypoints[j][2]]
-        cv2.imwrite(f'{output_folder}/{image_stem}_{j}.jpg',cropped_image)
+for j in range(len(keypoints)):
+    cropped_image=image[keypoints[j][1]:keypoints[j][3],keypoints[j][0]:keypoints[j][2]]
+    cv2.imwrite(f'{output_folder}/{image_stem}_{j}.jpg',cropped_image)
     
 
