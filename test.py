3413cbffd8c61633b9f24ce374d7b92ccfac7f00
Abdumannon0406 <halimovabdumannon0406@gmail.com>
2025-12-11 17:53:02 +0500

    train.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 model=YOLO('runs/detect/train11/weights/best.pt')
 
-results=model.track('/mnt/data11/projects/Shelf_retail/DATASET/testing_videos/WIN_20251128_14_01_07_Pro.mp4',tracker='bytetrack.yaml',save=True,show=True)
+results=model.track('/mnt/data11/projects/Shelf_retail/DATASET/extra_videos/WIN_20251128_13_58_02_Pro.mp4',tracker='bytetrack.yaml',save=True,show=True)
 
 # conf=results[0].boxes.conf.cpu().numpy()
 
